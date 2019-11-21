9ee2e41877a18a80e98367024c3a3762ca93419f
Mikko Johansson <mikko.johansson2@gmail.com>
2019-11-21 09:21:43 +0200

    New excercise

---
+++ rest_lang_jpa/src/main/java/com/example/demo/Lang.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+public class Lang {
+
+	private String name;
+	private String code;
+	
+	public Lang() {
+		
+	}
+	
+	public Lang(String name, String code) {
+		this.name = name;
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	@Override
+	public String toString() {
+		return "Lang [name=" + name + ", code=" + code + "]";
+	}
+	
+	
+}
