228782213cf342141f0acc28dfe5b52f95d8d953
Mikko Johansson <mikko.johansson2@gmail.com>
2019-11-21 14:04:41 +0200

    added PostgreSQL

--- rest_lang_jpa_H2/src/main/java/com/example/demo/repository/LangRepository.java
+++ rest_lang_jpa_H2/src/main/java/com/example/demo/repository/LangRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.Lang;
+@Repository
 public interface LangRepository extends CrudRepository<Lang, String> {
 
 }
